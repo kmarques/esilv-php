5c54cc99a97f8fd3b59d772e8adb1c8de2fa18e6
Karl MARQUES <kmarques@vetixy.com>
2023-09-29 16:03:34 +0200

    BIN2: add loop examples

---
+++ BIN2/mon-site/demo.php
@@ -0,0 +1,79 @@
+<?php
+
+$var1 = "Hello";
+$var2 = "World";
+function display (string $message): void {
+    echo $message;
+}
+
+class Toto {
+    public string $name;
+}
+
+echo $var1 . " " . $var2;
+
+
+
+function deletePost(int $postId):bool {
+    $role = "user";
+
+    if ($role === "admin") {
+        return true;
+    } else if ($role === "moderator" && $postId < 100){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function deletePostSwitch(int $postId):bool {
+    $role = "moderator";
+    $decision = null;
+    switch($role) {
+        case "admin":
+            $decision = true;
+            break;
+        case "moderator":
+            if ($postId < 100) {
+                $decision = true;
+            } else {
+                $decision = false;
+            }
+            break;
+        default:
+            $decision = false;
+            break;
+    }
+
+    return $decision;
+}
+
+function isModeratorValid($role, $postId) {
+    if($role === "moderator" && $postId < 100) {
+        return "moderator";
+    } else {
+        return null;
+    }
+    // return $role === "moderator" && $postId < 100 ? "moderator" : null;
+}
+
+function deletePostMatch(int $postId): bool {
+    $role = "moderator";
+    $decision = match($role) {
+        "admin" => true,
+        isModeratorValid($role, $postId) => true,
+        default => false
+    };
+
+    return $decision;
+}
+
+echo "Post deleted : " . deletePostMatch(100);
+
+
+$i = 0;
+
+echo ++$i . " <= ++i";
+echo $i . " <= i";
+echo $i++ . " <= i++";
+echo $i . " <= i";
