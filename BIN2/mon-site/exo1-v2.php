10b7e6f8e4ca446a6f8e9bfd83771125f0f94f49
Karl MARQUES <karl@kmarques.dev>
2023-10-24 17:11:35 +0200

    BIN2: gestion form

--- BIN2/mon-site/exo1-v2.php
+++ BIN2/mon-site/exo1-v2.php
@@ -1,5 +1,12 @@
 <?php
 
+if (!function_exists('str_starts_with')) {
+    function str_starts_with(string $haystack, string $needle)
+    {
+        return strpos($haystack, $needle) === 0;
+    }
+}
+
 function printCompleted($status)
 {
     return $status ? 'completed' : 'not completed';
